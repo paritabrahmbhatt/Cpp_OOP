b2a82cca14ff7dd198c792bea9ecad8c54d2ce66
Parita Brahmbhatt <67082863+paritabrahmbhatt@users.noreply.github.com>
2022-10-21 12:47:06 -0400

    Create P023_dynamicMemoryAllo.cpp

---
+++ P023_dynamicMemoryAllo.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int size;
+    int *arr;
+    cout<<"Enter the size of aaray"<<endl;
+    cin>>size;
+    arr = new int[size];
+    cout<<"Enter the elements of aaray"<<endl;
+    for(int i=0; i<size;i++){
+        cin>>arr[i];
+    }
+    cout<<"You have entered: "<<endl;
+    for(int i=0; i<size;i++){
+        cout<<arr[i]<<endl;
+    }
+
+    return 0;
+}
